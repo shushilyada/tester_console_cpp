621bd4b8973e5b575db5943e876163ad8bc8c75e
dnayr <d-nayr@outlook.com>
2017-02-21 05:38:26 -0500

    Add project files.

---
+++ tester_console_cpp/tester_console_cpp.cpp
@@ -0,0 +1,15 @@
+// tester_console_cpp.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Testing" << endl;
+	
+	return 0;
+}
+
